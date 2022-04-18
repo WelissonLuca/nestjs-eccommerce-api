295e22b76a84a96a114b18919d6656d2b134154e
Welisson luca <welissonluca17@gmail.com>
2022-04-18 14:37:09 -0300

    refactor: remove duplicated code

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -6,6 +6,11 @@ import { Validator } from '../../validators/validator';
 
 describe('UsersService', () => {
   let service: UsersService;
+  const createUserDto: CreateUserDto = {
+    name: internet.userName(),
+    email: internet.email(),
+    password: internet.password(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,68 +26,50 @@ describe('UsersService', () => {
 
   describe('createUser', () => {
     it('should not create user if name is null', () => {
-      const user = {
+      const promise = service.createUser({
+        ...createUserDto,
         name: '',
-        email: internet.email(),
-        password: internet.password(),
-      } as CreateUserDto;
-
-      const promise = service.createUser(user);
+      });
       expect(promise).rejects.toThrowError('name is required');
     });
 
     it('should not create user if email is null', () => {
-      const user = {
-        name: internet.userName(),
+      const promise = service.createUser({
+        ...createUserDto,
         email: '',
-        password: internet.password(),
-      } as CreateUserDto;
-
-      const promise = service.createUser(user);
+      });
       expect(promise).rejects.toThrowError('email is required');
     });
     it('should not create user if password is null', () => {
-      const user = {
-        name: internet.userName(),
-        email: internet.email(),
+      const promise = service.createUser({
+        ...createUserDto,
         password: '',
-      } as CreateUserDto;
-
-      const promise = service.createUser(user);
+      });
       expect(promise).rejects.toThrowError('password is required');
     });
     it('should not create a user if email is not valid', () => {
-      const user = {
-        email: 'invalid-email',
-        password: internet.password(),
-        name: internet.userName(),
-      };
-
-      const promise = service.createUser(user);
+      const promise = service.createUser({
+        ...createUserDto,
+        email: 'invalidEmail',
+      });
       expect(promise).rejects.toThrowError('Email is not valid');
     });
 
     it('should not create a user if password less than 6 characters', () => {
-      const user = {
-        email: internet.email(),
-        password: '123',
-        name: internet.userName(),
-      };
-
-      const promise = service.createUser(user);
+      const promise = service.createUser({
+        ...createUserDto,
+        password: '12345',
+      });
       expect(promise).rejects.toThrowError(
         'Password must be between 6 and 20 characters',
       );
     });
 
     it('should not create a user if password longer than 20 characters', () => {
-      const user = {
-        email: internet.email(),
+      const promise = service.createUser({
+        ...createUserDto,
         password: internet.password(21),
-        name: internet.userName(),
-      };
-
-      const promise = service.createUser(user);
+      });
       expect(promise).rejects.toThrowError(
         'Password must be between 6 and 20 characters',
       );
