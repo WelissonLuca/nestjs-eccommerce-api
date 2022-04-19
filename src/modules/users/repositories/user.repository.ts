e10f64f58047b1b1887610b8f1c574ec6710631e
Welisson luca <welissonluca17@gmail.com>
2022-04-19 15:15:10 -0300

    fix: user controller spec

--- src/modules/users/repositories/user.repository.ts
+++ src/modules/users/repositories/user.repository.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepositoryContracts } from '../contracts/user-repository.contract';
+import { UpdateUserDto } from '../dtos/update-user.dto';
 
 @Injectable()
 export class UserRepository implements UserRepositoryContracts {
+  update(data: UpdateUserDto, email: string): Promise<any> {
+    return;
+  }
   findOneByEmail(email: string): Promise<any> {
     return;
   }
