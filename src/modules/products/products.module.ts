b251f37223fbd3fc7ea2929cebec3a6d22a76369
Welisson luca <welissonluca17@gmail.com>
2022-04-30 13:55:18 -0300

    fix: import product repository

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,11 +1,11 @@
+import { ProductRepository } from './repositories/product.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-import { Product } from './entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([ProductRepository])],
   controllers: [ProductsController],
   providers: [
     {
